ea0be7562487e851c12548d60ba70131a22c36f6
Khiem <khiem17204@gmail.com>
2022-12-13 11:07:56 -0500

    add search component

---
+++ src/components/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2851420c61msh87fd12158e363f2p1625fdjsnc4a18e28497a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
